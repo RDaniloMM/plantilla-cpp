9c15695ad1e7a9dad28a12388140195eadbc52f6
Rivaldo Danilo Morón Maylle <93401578+RDaniloMM@users.noreply.github.com>
2024-10-23 14:43:11 -0500

    a

---
+++ plantilla.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define loop(i,a) for(uint i=0;i<a;++i)
+#define loop2(i,a,b) for(uint i=a;i<b;++i)
+#define rloop(i,a,b) for(uint i=a;i>=b;--i)
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+#define sp ' '
+#define bgn begin
+#define all(a) a.bgn(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define b_s binary_search
+#define MOD 1000000007
+#define printv(a) {for(auto u:a) cout<<u<<sp; cout<<endl;}
+#define printm(a) {for(auto u:a) cout<<u.ff <<sp<< u.ss<<endl;}
+using namespace std;
+
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<uint> vui;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<int>::iterator vit;
+typedef map<int, int> mii;
+typedef map<char, char> mcc;
+typedef set<int> si;
+typedef set<char> sc;
+
+int main(){
+    fio;
+    
+    return 0;
+}
